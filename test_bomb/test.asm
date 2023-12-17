2a92109fea1ef08dacc468a3f92e80194fc08769
594cofee <594coffee@gmail.com>
2023-12-18 04:35:59 +0800

    還是有Bug的第三隻鬼

--- test_bomb/test.asm
+++ test_bomb/test.asm
@@ -25,6 +25,7 @@ counterBomb DD 0				;炸彈計時器初始為0
 counterExplosion DD 0			;爆炸計時器初始為0
 counterEnemy1 DD 0				;敵人1計時器初始為0
 counterEnemy2 DD 0				;敵人2計時器初始為0
+counterEnemy3 DD 0				;敵人3計時器初始為0
 counterMap DD 0					;地圖編號初始為0
 map_x DD 0
 map_y DD 0
@@ -71,13 +72,20 @@ enemy1_alive DD 1, 1						;檢查敵人1是否還活著
 enemy2_x DD area_width-200;,area_width-500	;敵人2初始位置
 enemy2_y DD area_height-300;,area_width-500
 enemy2_alive DD 1, 1						;檢查敵人2是否還活著
+enemy3_x DD area_width-400;,area_width-500	;敵人3初始位置
+enemy3_y DD area_height-300;,area_width-500
+enemy3_alive DD 1, 1						;檢查敵人3是否還活著
+
 game_over_check DD 0						;檢查玩家是否輸了
 pass_wall DD 0                                ;檢查是否可以穿牆
 lastdoor DD 0                                ;儲存人物移動的上一個區塊顏色
 lastwall DD 0                                ;儲存人物移動的上一個區塊顏色
 lasttool1 DD 0                                ;儲存人物移動的上一個區塊顏色
 lasttool2 DD 0                                ;儲存人物移動的上一個區塊顏色
+lasttool3 DD 0                                ;儲存人物移動的上一個區塊顏色
 last DD 0                                ;儲存人物是否站在牆上
+door_x DD 0
+door_y DD 0
 
 aux DD 0			;輔助變數
 aux1 DD 0
@@ -277,7 +285,7 @@ local up, left, down, right, finish
 endm
 
 press_button macro x,y,button_x,button_y, diff_x, diff_y	;檢查方向按鈕按下情況的巨集
-local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantpass, canpass1, canpass2, canpass3, canpass4, iswall, drawdoor, drawwall, drawtool1, drawtool2
+local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantpass, canpass1, canpass2, canpass3, canpass4, iswall, drawdoor, drawwall, drawtool1, drawtool2, get_tool3, canpass5, drawtool3
 	mov eax, x
 	cmp eax, button_x
 	jl button_fail
@@ -297,6 +305,10 @@ local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantp
 	cmp dword ptr [eax], 00069B4h
 	je defeat
 
+	calculate_pozition bomberman_x,bomberman_y,diff_x, diff_y	;遇到敵人3
+	cmp dword ptr [eax], 0AA69CCh
+	je defeat
+
 	calculate_pozition bomberman_x,bomberman_y,diff_x, diff_y	;遇到門
 	cmp dword ptr [eax], 000FF00h
 	je to_next
@@ -309,6 +321,10 @@ local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantp
 	cmp dword ptr [eax], 0A300E5h
 	je get_tool2
 
+	calculate_pozition bomberman_x,bomberman_y,diff_x, diff_y	;遇到道具3
+	cmp dword ptr [eax], 0CAFFE5h
+	je get_tool3
+
 	calculate_pozition bomberman_x,bomberman_y,diff_x, diff_y
 	cmp pass_wall, 0
 	je cantpass
@@ -320,6 +336,8 @@ local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantp
 	je canpass3
 	cmp dword ptr [eax], 0A0532Dh			;遇到牆(道具2)
 	je canpass4
+	cmp dword ptr [eax], 0A0512Dh			;遇到牆(道具3)
+	je canpass5
 	cmp last, 1
 	je iswall
 	jmp cantpass
@@ -348,6 +366,12 @@ local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantp
 	mov lasttool2, 1
 	jmp road
 
+	canpass5:
+	mov last, 1
+	mov bomb_check, 1
+	mov lasttool3, 1
+	jmp road
+
 	cantpass:
 	cmp dword ptr [eax], 0FFFFFFh
 	jne button_fail
@@ -374,6 +398,8 @@ local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantp
 	je drawtool1
 	cmp lasttool2, 1
 	je drawtool2
+	cmp lasttool3, 1
+	je drawtool3
 	jmp bomb_case
 	
 	drawdoor:
@@ -396,10 +422,23 @@ local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantp
 	mov lasttool2, 0
 	jmp bomb_case
 
+	drawtool3:
+	draw_square bomberman_x, bomberman_y, 0A0512Dh
+	mov lasttool3, 0
+	jmp bomb_case
+
 	bomb_case:
 	add bomberman_y, diff_y
 	add bomberman_x, diff_x
 	draw_square bomberman_x, bomberman_y, 0FF0000h
+	mov eax, bomberman_x
+	add eax, 2
+	mov aux, eax
+	mov eax, bomberman_y
+	add eax, 2
+	mov aux1, eax
+	make_text_macro 'A',area, aux, aux1
+
 	jmp button_fail
 	
 	defeat:
@@ -416,6 +455,10 @@ local button_fail, bomb_case, defeat, to_next, get_tool1, road, get_tool2, cantp
 
 	get_tool2:
 		tool2
+		jmp road
+
+	get_tool3:
+		tool3
 		jmp road
 
 	button_fail:
@@ -466,9 +509,21 @@ tool2 macro                ;獲得道具2的巨集
     mov pass_wall, 1
 endm
 
+tool3 macro                ;獲得道具3的巨集
+    draw_square door_x, door_y, 000FF00h
+endm
+
 next_level macro            ;切換下一關的巨集
+local has_next, nextend
+    cmp counterMap, 2
+    jne has_next
+    win_game
+    jmp nextend
+
+    has_next:
     mov enemy1_alive,1
     mov enemy2_alive,1
+	mov enemy3_alive,1
     mov bomb_check,0 
 
     ; 清空當前地圖
@@ -483,17 +538,48 @@ next_level macro            ;切換下一關的巨集
     add esp, 12
 
     ; 在這裡添加生成下一關地圖的代碼
-	inc counterMap
+    inc counterMap
     call draw
 
     ; 更新遊戲狀態，表示下一關開始
     mov game_over_check, 0   ; 這裡將遊戲狀態設置為未結束
+
+    nextend:
+
+endm
+
+win_game macro                ;遊戲獲勝的巨集
+    mov enemy1_alive,0
+    mov enemy2_alive,0
+	mov enemy3_alive,0
+    mov bomb_check,0 
+
+    mov eax, area_width
+    mov ebx, area_height
+    mul ebx
+    shl eax, 2
+    push eax
+    push 0A7A6A5h            ;整個地圖塗成灰色
+    push area
+    call memset
+    add esp, 12
+
+    make_text_macro 'D', area, area_width/2-25, area_height/2
+    make_text_macro 'E', area, area_width/2-15, area_height/2
+    make_text_macro 'F', area, area_width/2-5, area_height/2
+    make_text_macro 'E', area, area_width/2+5, area_height/2
+    make_text_macro 'A', area, area_width/2+15, area_height/2
+    make_text_macro 'T', area, area_width/2+25, area_height/2
+
+    mov game_over_check, 1
+
 
 endm
 
 game_over macro				;遊戲結束的巨集
 	mov enemy1_alive,0
 	mov enemy2_alive,0
+	mov enemy3_alive,0
 	mov bomb_check,0 
 
 	mov eax, area_width
@@ -519,7 +605,7 @@ game_over macro				;遊戲結束的巨集
 endm
 
 explosion_radius macro x, y, diff_x, diff_y, color        ;受爆炸影響的區域的巨集
-local unbreakable, explosion_loop, clear_loop, breakable, crate, defeat, enemy1, enemy2, open_door, take_tool1, take_tool2
+local unbreakable, explosion_loop, clear_loop, breakable, crate, defeat, enemy1, enemy2, enemy3,open_door, take_tool1, take_tool2, take_tool3
     mov ESI, bomb_x
     mov aux1, ESI
     mov ESI, bomb_y
@@ -536,6 +622,8 @@ local unbreakable, explosion_loop, clear_loop, breakable, crate, defeat, enemy1,
     je enemy1
     cmp dword ptr [eax], 00069B4h    ;敵人2
     je enemy2
+	cmp dword ptr [eax], 0AA69CCh    ;敵人3
+    je enemy3
     cmp dword ptr [eax], 0FF0000h    ;玩家
     je defeat
     cmp dword ptr [eax], 0A0522Dh    ;磚
@@ -546,6 +634,8 @@ local unbreakable, explosion_loop, clear_loop, breakable, crate, defeat, enemy1,
     je take_tool1
 	cmp dword ptr [eax], 0A0532Dh    ;磚(有道具2)
     je take_tool2
+	cmp dword ptr [eax], 0A0512Dh    ;磚(有道具3)
+    je take_tool3
     cmp dword ptr [eax], 0F59B00h    ;爆炸
     je breakable
     cmp dword ptr [eax], 0FFFFFFh    ;路
@@ -571,6 +661,12 @@ local unbreakable, explosion_loop, clear_loop, breakable, crate, defeat, enemy1,
 	add x, diff_x
     add y, diff_y
     draw_square x, y, 0A300E5h
+    jmp unbreakable
+
+	take_tool3:
+	add x, diff_x
+    add y, diff_y
+    draw_square x, y, 0CAFFE5h
     jmp unbreakable
 
     defeat:
@@ -601,6 +697,13 @@ local unbreakable, explosion_loop, clear_loop, breakable, crate, defeat, enemy1,
     add y, diff_y
     draw_square x, y, 0FFFFFFh
     mov enemy2_alive,0
+	jmp unbreakable
+
+	enemy3:
+    add x, diff_x
+    add y, diff_y
+    draw_square x, y, 0FFFFFFh
+    mov enemy3_alive,0
 	jmp unbreakable
 
 	clear_loop:
@@ -705,7 +808,7 @@ local canbomb
 endm
 
 create_map macro		;製作地圖的巨集
-local done, loop_, wall, crate, road, door, tool1, tool2, tool3, enemy1, enemy2, next
+local done, loop_, wall, crate, road, door, tool1, tool2, tool3, enemy1, enemy2, enemy3, next
 random
 ;磚的位置不能同時為100倍(以及50,50、50,100、100,50不要有)
 	push ebp
@@ -738,10 +841,16 @@ random
 	je enemy1
 	cmp byte ptr [esi],8
 	je enemy2
+	cmp byte ptr [esi],9
+	je enemy3
 	
 
 	door:
 	draw_square map_x, map_y, 0A0522Eh
+	mov edx, map_x
+	mov door_x, edx
+	mov edx, map_y
+	mov door_y, edx
 	jmp next
 
 	wall:
@@ -765,7 +874,7 @@ random
 	jmp next
 	
 	tool3:
-	;draw_square map_x, map_y, 0A0532Dh
+	draw_square map_x, map_y, 0A0512Dh
 	jmp next
 
 	enemy1:
@@ -782,6 +891,14 @@ random
 	mov eax,map_y
 	mov enemy2_y, eax
 	draw_square map_x, map_y, 00069B4h
+	jmp next
+
+	enemy3:
+	mov eax,map_x
+	mov enemy3_x, eax
+	mov eax,map_y
+	mov enemy3_y, eax
+	draw_square map_x, map_y, 0AA69CCh
 	jmp next
 
 	next:
@@ -867,7 +984,7 @@ local up,left,down,right, skip, defeat, reroll, no_movement, reset
 	mov aux1, -50
 	calculate_pozition enemy1_x,enemy1_y,aux, aux1
 	cmp dword ptr [eax], 0FFFFFFh
-	jne reroll
+	jne left
 	jmp skip
 	
 	left:
@@ -875,7 +992,7 @@ local up,left,down,right, skip, defeat, reroll, no_movement, reset
 	mov aux1, 0
 	calculate_pozition enemy1_x,enemy1_y,aux, aux1
 	cmp dword ptr [eax], 0FFFFFFh
-	jne reroll
+	jne right
 	jmp skip
 	
 	right:
@@ -883,7 +1000,7 @@ local up,left,down,right, skip, defeat, reroll, no_movement, reset
 	mov aux1, 0
 	calculate_pozition enemy1_x,enemy1_y,aux, aux1
 	cmp dword ptr [eax], 0FFFFFFh
-	jne reroll
+	jne down
 	jmp skip
 	
 	down:
@@ -891,7 +1008,7 @@ local up,left,down,right, skip, defeat, reroll, no_movement, reset
 	mov aux1, 50
 	calculate_pozition enemy1_x,enemy1_y,aux, aux1
 	cmp dword ptr [eax], 0FFFFFFh
-	jne reroll
+	jne up
 	jmp skip
 	
 	skip:
@@ -942,21 +1059,33 @@ local up,left,down,right, skip, defeat, reroll, no_movement, reset
 	up:
 	mov aux, 0
 	mov aux1, -50
+	calculate_pozition enemy2_x,enemy2_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne left
 	jmp skip
 	
 	left:
 	mov aux, -50
 	mov aux1, 0
+	calculate_pozition enemy2_x,enemy2_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne right
 	jmp skip
 	
 	right:
 	mov aux, 50
 	mov aux1, 0
+	calculate_pozition enemy2_x,enemy2_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne down
 	jmp skip
 	
 	down:
 	mov aux, 0
 	mov aux1, 50
+	calculate_pozition enemy2_x,enemy2_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne up
 	jmp skip
 	
 	skip:
@@ -977,6 +1106,83 @@ local up,left,down,right, skip, defeat, reroll, no_movement, reset
 	mov eax, aux1
 	add enemy2_y, eax
 	draw_square enemy2_x, enemy2_y, 00069B4h
+	jmp no_movement
+	
+	defeat:
+	game_over
+	
+	no_movement:
+	
+endm
+
+enemy3_movement macro		;決定敵人3行動的巨集
+local up,left,down,right, skip, defeat, reroll, no_movement, reset
+	inc counterEnemy3
+	cmp counterEnemy3, 5
+	jne no_movement
+	
+	reroll:	
+	random
+	
+	cmp edx, 0
+	je up
+	cmp edx, 1
+	je left
+	cmp edx, 2
+	je down
+	cmp edx, 3
+	je right
+	
+	up:
+	mov aux, 0
+	mov aux1, -50
+	calculate_pozition enemy3_x,enemy3_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne left
+	jmp skip
+	
+	left:
+	mov aux, -50
+	mov aux1, 0
+	calculate_pozition enemy3_x,enemy3_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne right
+	jmp skip
+	
+	right:
+	mov aux, 50
+	mov aux1, 0
+	calculate_pozition enemy3_x,enemy3_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne down
+	jmp skip
+	
+	down:
+	mov aux, 0
+	mov aux1, 50
+	calculate_pozition enemy3_x,enemy3_y,aux, aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne up
+	jmp skip
+	
+	skip:
+	mov counterEnemy3,0
+	calculate_pozition enemy3_x,enemy3_y,aux, aux1
+	cmp dword ptr [eax], 0FF0000h
+	je defeat
+	
+	calculate_pozition enemy3_x,enemy3_y,aux,aux1
+	cmp dword ptr [eax], 0FFFFFFh
+	jne reroll
+	
+	reset:
+	draw_square enemy3_x, enemy3_y, 0FFFFFFh
+	
+	mov eax, aux
+	add enemy3_x, eax
+	mov eax, aux1
+	add enemy3_y, eax
+	draw_square enemy3_x, enemy3_y, 0AA69CCh
 	jmp no_movement
 	
 	defeat:
@@ -1111,12 +1317,16 @@ evt_timer:
 	cmp enemy2_alive,1
 	jne final_draw
 	enemy2_movement
-	;jmp final_draw
+	jmp final_draw
 
-	;count:
-	;sub enemy_num, 1 
-	;cmp enemy_num, 0
-	;jmp final_draw
+	enemy3:
+	cmp enemy3_alive,1
+	jne final_draw
+	enemy3_movement
+	cmp enemy3_alive,1
+	jne final_draw
+	enemy3_movement
+	jmp final_draw
 
 	final_draw:
 	popa
